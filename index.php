9cf7e9fb802062006b38964dde5a980b3664879d
arte00 <hubi.lukki@gmail.com>
2022-10-17 18:35:59 +0200

    database connection, entities, routing, render html

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
+
+
 <html>
  <head>
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+ <?php echo 'xd'; ?>
  </body>
 </html>
---
+++ pogodynka/src/Entity/Measurement.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MeasurementRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MeasurementRepository::class)]
+class Measurement
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column]
+    private ?float $temperatureCelsius = null;
+
+    #[ORM\Column]
+    private ?float $windSpeed = null;
+
+    #[ORM\Column]
+    private ?float $pressure = null;
+
+    #[ORM\Column]
+    private ?float $humidity = null;
+
+    #[ORM\Column]
+    private ?int $weatherType = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?location $location = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getTemperatureCelsius(): ?float
+    {
+        return $this->temperatureCelsius;
+    }
+
+    public function setTemperatureCelsius(float $temperatureCelsius): self
+    {
+        $this->temperatureCelsius = $temperatureCelsius;
+
+        return $this;
+    }
+
+    public function getWindSpeed(): ?float
+    {
+        return $this->windSpeed;
+    }
+
+    public function setWindSpeed(float $windSpeed): self
+    {
+        $this->windSpeed = $windSpeed;
+
+        return $this;
+    }
+
+    public function getPressure(): ?float
+    {
+        return $this->pressure;
+    }
+
+    public function setPressure(float $pressure): self
+    {
+        $this->pressure = $pressure;
+
+        return $this;
+    }
+
+    public function getHumidity(): ?float
+    {
+        return $this->humidity;
+    }
+
+    public function setHumidity(float $humidity): self
+    {
+        $this->humidity = $humidity;
+
+        return $this;
+    }
+
+    public function getWeatherType(): ?int
+    {
+        return $this->weatherType;
+    }
+
+    public function setWeatherType(int $weatherType): self
+    {
+        $this->weatherType = $weatherType;
+
+        return $this;
+    }
+
+    public function getLocation(): ?location
+    {
+        return $this->location;
+    }
+
+    public function setLocation(?location $location): self
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+}
